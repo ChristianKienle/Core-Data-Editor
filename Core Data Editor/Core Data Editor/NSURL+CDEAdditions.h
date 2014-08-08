bad3a15c4a9bd9e23758e598ad7d4f11e4b6072a
Daniele Orrù <daniele.orru.dev@gmail.com>
2014-08-09 00:01:58 +0200

    Create CDEProjectBrowserItem only if Application folder matches for storePath and modelPath

--- Core Data Editor/Core Data Editor/NSURL+CDEAdditions.h
+++ Core Data Editor/Core Data Editor/NSURL+CDEAdditions.h
@@ -8,6 +8,15 @@
 
 #pragma mark - UTI
 - (NSString *)typeOfFileURLAndGetError_cde:(NSError **)error;
+/**
+ * Return the Application name if the NSURL contains it. nil if not found
+ * Example:
+ *  Input:  /iPhone Simulator/7.1/Applications/412F2A6B-0F54-4F98-AE1B-DFE99E057DB1/Example.app/v1.momd/v5.mom
+ *  Output: 412F2A6B-0F54-4F98-AE1B-DFE99E057DB1
+ *
+ * @return NSString*
+ */
+- (NSString *)appFolderName_cde;
 - (BOOL)isCompiledManagedObjectModelFile_cde;
 
 #pragma mark - App Specific URLs
