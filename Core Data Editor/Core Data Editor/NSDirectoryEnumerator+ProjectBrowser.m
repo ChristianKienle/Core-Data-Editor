9cbf771f152cd7aee75c931b6aa42caee5cffb2e
Christian Kienle <me@christian-kienle.de>
2014-08-10 18:25:48 +0200

    NSURL addtions

--- Core Data Editor/Core Data Editor/NSDirectoryEnumerator+ProjectBrowser.m
+++ Core Data Editor/Core Data Editor/NSDirectoryEnumerator+ProjectBrowser.m
@@ -1,40 +1,80 @@
 #import "NSDirectoryEnumerator+ProjectBrowser.h"
 #import "NSURL+CDEAdditions.h"
 
+@implementation NSURL (ProjectBrowser)
 
+- (NSManagedObjectModel *)transformedManagedObjectModel {
+    BOOL isModel = [self isCompiledManagedObjectModelFile_cde];
+    if(isModel == NO) {
+        return nil;
+    }
+    
+    // try-catch because it is very well possible that a file seems to be a managed object model
+    // but it isn't or something else goes wrong when we initialize an NSManagedObjectModel with
+    // (unknown) contents of a file.
+    NSManagedObjectModel *transformedModel;
+    @try {
+        NSManagedObjectModel *model = [[NSManagedObjectModel alloc] initWithContentsOfURL:self];
+        if(model == nil) {
+            return nil;
+        }
+        transformedModel = model.transformedManagedObjectModel_cde;
+        if(transformedModel == nil) {
+            return nil;
+        }
+    }
+    @catch (NSException *exception) {
+        
+    }
+    @finally {
+        
+    }
+    return transformedModel;
+}
+
+- (NSDictionary *)persistentStoreMetadata {
+    BOOL isData = [self isPublicDataFile_cde];
+    if(isData == NO) {
+        return nil;
+    }
+    
+    if([self isSQLiteURL_cde] == NO) {
+        return nil;
+    }
+    NSDictionary *metadata;
+    @try {
+        NSError *error;
+        // +metadataForPersistentStoreOfType:URL:error: fails if we have a SQLite file but it is not a Core Data SQLite file...
+        metadata = [NSPersistentStoreCoordinator metadataForPersistentStoreOfType:nil URL:self error:&error];
+        if(metadata == nil) {
+            return nil;
+        }
+    }
+    @catch (NSException *exception) {
+        
+    }
+    @finally {
+        
+    }
+    return metadata;
+}
+
+@end
 
 @implementation NSDirectoryEnumerator (ProjectBrowser)
 
 - (void)getMetadataByStorePath:(NSDictionary **)outMetadataByStorePath modelByModelPath:(NSDictionary **)outModelByModelPath {
     NSMutableDictionary *metadataByStorePath = [NSMutableDictionary new];
     NSMutableDictionary *modelByModelPath = [NSMutableDictionary new];
-
+    
     for(NSURL *URL in self) {
-        
-
-        BOOL isModel = [URL isCompiledManagedObjectModelFile_cde];
-        if(isModel) {
-            @try {
-                NSManagedObjectModel *model = [[NSManagedObjectModel alloc] initWithContentsOfURL:URL];
-                if(model == nil) {
-                    continue;
-                }
-                NSManagedObjectModel *transformedModel = model.transformedManagedObjectModel_cde;
-                if(transformedModel == nil) {
-                    continue;
-                }
-                modelByModelPath[URL.path] = transformedModel;
-            }
-            @catch (NSException *exception) {
-                
-            }
-            @finally {
-                
-            }
-            
+        // Is URL a managed object model file?
+        NSManagedObjectModel *model = [URL transformedManagedObjectModel];
+        if(model != nil) { // YES
+            modelByModelPath[URL.path] = model;
             continue;
         }
-        
+                
         BOOL isData = [URL isPublicDataFile_cde];
         if(isData == NO) {
             continue;
@@ -44,14 +84,13 @@
             continue;
         }
         
-        NSError *error;
-        NSDictionary *metadata = [NSPersistentStoreCoordinator metadataForPersistentStoreOfType:nil URL:URL error:&error];
+        NSDictionary *metadata = [URL persistentStoreMetadata];
         if(metadata == nil) {
             continue;
         }
         metadataByStorePath[URL.path] = metadata;
     }
-
+    
     if(outMetadataByStorePath != NULL) {
         *outMetadataByStorePath = metadataByStorePath;
     }
