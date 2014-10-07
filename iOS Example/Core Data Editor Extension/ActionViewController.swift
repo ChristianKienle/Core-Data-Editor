ed950acb80998b2555a86a9c4bafd61eb49af724
talzag <dstrokis@gmail.com>
2014-10-07 00:57:46 -0400

    Removed some unnecessary code from the app extension.

--- iOS Example/Core Data Editor Extension/ActionViewController.swift
+++ iOS Example/Core Data Editor Extension/ActionViewController.swift
@@ -10,49 +10,15 @@ import UIKit
 import MobileCoreServices
 
 class ActionViewController: UIViewController {
-
-    @IBOutlet weak var imageView: UIImageView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Get the item[s] we're handling from the extension context.
-        
-        // For example, look for an image and place it into an image view.
-        // Replace this with something appropriate for the type[s] your extension supports.
-        var imageFound = false
+
         for item: AnyObject in self.extensionContext!.inputItems {
             let inputItem = item as NSExtensionItem
             for provider: AnyObject in inputItem.attachments! {
                 let itemProvider = provider as NSItemProvider
-                if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeImage as NSString) {
-                    // This is an image. We'll load it, then place it in our image view.
-                    weak var weakImageView = self.imageView
-                    itemProvider.loadItemForTypeIdentifier(kUTTypeImage as NSString, options: nil, completionHandler: { (image, error) in
-                        if image != nil {
-                            NSOperationQueue.mainQueue().addOperationWithBlock {
-                                if let imageView = weakImageView {
-                                    imageView.image = image as? UIImage
-                                }
-                            }
-                        }
-                    })
-                    
-                    imageFound = true
-                    break
-                }
-            }
-            
-            if (imageFound) {
-                // We only handle one image, so stop looking for more.
-                break
             }
         }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func done() {
